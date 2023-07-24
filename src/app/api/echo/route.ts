c94f173caaf1cba56358878b49017d37e0979dd9
Joshua Maffuccio <joshuamaffuccio@gmail.com>
2023-07-24 10:53:42 -0700

    Add post route

--- src/app/api/echo/route.ts
+++ src/app/api/echo/route.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 
+// * routes are dynamic when we use Request object with GET
+// * Any other verbs will automatically be dynamic
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const name = searchParams.get("name")
-  const instrument = searchParams.get("instrument")
+  const obj = Object.fromEntries(searchParams.entries())
 
-  return NextResponse.json({
-    name, instrument
-  })
+  return NextResponse.json(obj)
 }
